efccc5e4b9906a201d3221755277702876e98fe8
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-09-02 13:20:40 +0330

    first commit

---
+++ contract/SimpleStorage.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    uint favouriteNumber = 10;
+
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    Person public person = Person({
+        favouriteNumber: 10,
+        name: "John"
+    });
+
+    struct Person {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    Person[] public people;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    function add() public pure returns(uint256) {
+        return (1 + 1);
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(Person({
+            favouriteNumber: _favouriteNumber,
+            name: _name
+        }));
+
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
